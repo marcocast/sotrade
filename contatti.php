191090379f0ca86a6882bfcfb7fde0fd5fafc139
marcocast <marcocast@gmail.com>
2014-12-04 21:09:03 +0000

    adding all

---
+++ contatti.php
@@ -0,0 +1,266 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+	
+	<title>Sotrade - la qualità  nei trasporti - contatti</title>
+	<meta name="description" content="felici di realizzare tutto cio di cui avete bisogno" />
+	<meta name="keywords" content="sotrade,sotrade srl,trasporto merci,trasporti,spedizioni,internazionali,import/export,Valencia,Belgio ,spagna,barcellona,Informazioni sul traffico ,castigliego" />
+	<meta name="identifier-url" content="www.sotrade.it" />
+	<meta name="category" content="Transports" />
+	<meta http-equiv="content-Language" content="it" />  
+	<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+
+
+
+	<link rel="stylesheet" href="style.css" type="text/css" charset="utf-8" />
+	<script src="script.js"></script>
+</head>
+
+<body>
+	<table border="0" cellpadding="0" cellspacing="0" width="100%" height="203">
+		<tr>
+				<td height="137" width="100%" background="images/titolo/header_r1_c4.jpg" colspan="3" align="left">
+					&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+					<img src="images/header/header_conScritta.jpg" width="500" height="137" border="0" alt="Sotrade Srl"></td>
+			</tr>
+			<tr>
+				<td height="43" width="100%" background="images/titolo/header_r2_c4.jpg" class="menu" align="center">
+				<table border="0" cellpadding="6" cellspacing="6" width="100%" height="43" valign="middle">
+					<tr>
+						<td width="5%">&nbsp;</td>
+						<td nowrap><a href="index.php" class="menu">Home</a></td>
+						<td nowrap><a href="servizi.php" class="menu">Servizi</a></td>
+						<td nowrap><a href="dove.php" class="menu">Dove siamo</a></td>
+						<td nowrap class="menuSel">Contattaci</a></td>
+						<td nowrap><a href="preventivi.php" class="menu">Preventivi</a></td>
+						<td nowrap><a href="lavoro.php" class="menu">Lavora con noi</a></td>
+						<td width="100%">&nbsp;</td>
+					</tr>                              
+				</table>    
+			</td>
+			<td height="43" width="159" background="images/titolo/header_r2_c5.jpg" nowrap>&nbsp;</td>
+			<td height="43" width="120" background="images/titolo/header_r2_c6.jpg" nowrap align="center">&nbsp;
+				<a href="index.php" class="menuSel"><img src="images/titolo/gb.gif" alt="English" width="18" height="12" border="0"></a>&nbsp;
+				<a href="index.php" class="menuSel"><img src="images/titolo/it.gif" alt="Italiano" width="18" height="12" border="0"></a>&nbsp;
+				<a href="indexSp.php" class="menuSel"><img src="images/titolo/es.gif" alt="Espa&ntilde;ol" width="18" height="12" border="0"></a>&nbsp;
+				<a href="#" class="menuSel"><img src="images/titolo/frGr.gif" alt="Fan&ccedil;ais" width="18" height="12" border="0"></a>
+			</td>
+		</tr>
+		<tr>
+			<td height="23" width="100%" background="images/titolo/header_r3_c4.jpg">&nbsp;</td>
+			<td height="23" width="159" background="images/titolo/header_r3_c5.jpg" nowrap>&nbsp;</td>
+			<td height="23" width="120" background="images/titolo/header_r3_c6.jpg" nowrap>&nbsp;</td>
+		</tr>
+	</table>
+
+	<div id="wrapper">
+		<div id="headline">
+			<div id="photo">
+			<div align="center">				
+				<img src="images/topcontatti.jpg" width="611" height="212" alt="Pic 1"></div></div>
+			<div id="search-news">
+				<h2>&nbsp;</h2>
+				
+				<h2 class="downcast">News &amp; Eventi</h2>
+				<?php include 'news.php'; ?>
+			</div>
+			<div class="clear"></div>
+		</div>
+		<div id="body">
+			<div id="body-left" style="width: 407; height: 1395">
+			
+			
+				<h2 style="text-align: center"><font size="5" color="#A2B849">
+                Per qualsiasi informazione</font></h2>
+				
+			
+				<p style="text-align: center"><b><font size="3">TEL&nbsp; : 
+                02/6460695</font></b></p>
+				
+			
+				<p style="text-align: center"><b><font size="3">FAX&nbsp; : 
+                02/6453120</font></b></p>
+				
+			
+                      
+                      
+				<p style="text-align: center">&nbsp;</p>
+				
+			
+                      
+                      
+				<p style="text-align: center"><i><font size="7">SOTRADE</font></i></p>
+				
+			
+                      
+                      
+				<p style="text-align: center"><i><font size="7">
+                <img border="0" src="images/AROBAZE.jpg" width="94" height="95"></font></i></p>
+				
+			
+                      
+                      
+				<p style="text-align: center"><i><font size="7">SOTRADE.IT</font></i></p>
+				
+			
+                      
+                      
+				<p style="text-align: center">&nbsp;</p>
+				
+			
+                      
+                      
+				<p style="text-align: center">&nbsp;</p>
+				
+			
+                      
+                      
+				<p style="text-align: center">
+                <img border="0" src="images/Crystal_128_mail.jpg" width="128" height="128" align="left"></p>
+				
+			
+                      
+                      
+				<p style="text-align: center">
+                &nbsp;</p>
+				
+			
+                      
+                      
+				<p style="text-align: center">
+                <font size="3"><b><u>EMAIL</u>: </b>
+                </font></p>
+				
+			
+                      
+                      
+				<p style="text-align: center">
+                &nbsp;</p>
+				
+			
+                      
+                      
+				<p style="text-align: center">
+                &nbsp;</p>
+				
+			
+                      
+                      
+				<p style="text-align: center">
+                &nbsp;</p>
+				
+			
+                      
+                      
+				<p style="text-align: center">
+                &nbsp;</p>
+				
+			
+                      
+                      
+                      <ul class="plussbullets">
+					<li><a href="mailto:a.castigliego@sotrade.it?subject=Web Mail">Reparto Aereo </a></li>
+					<li><a href="mailto:mare4@sotrade.it?subject=Web Mail">Reparto Marittimo</a></li>
+					<li><a href="mailto:sotrade@sotrade.it?subject=Web Mail">Per 
+                    informazioni</a></li>
+					<li><a href="mailto:a.castigliego@sotrade.it?subject=Web Mail">Reparto Commerciale</a></li>
+					<li><a href="mailto:comm@sotrade.it?subject=Web Mail">
+                    Reparto import/export Spagna-Barcellona</a></li>
+					<li><a href="mailto:lino@sotrade.it?subject=Web Mail">
+                    Reparto import/export Belgio</a></li>
+					<li><a href="mailto:sotrade@sotrade.it?subject=Web Mail">
+                    Reparto import/export Valencia</a></li>
+				</ul>
+
+                      
+				
+
+			
+                      
+                      
+				<p style="text-align: left">&nbsp;</p>
+				
+			
+                      
+                      
+				<p style="text-align: center">&nbsp;</p>
+				
+			
+                      
+                      
+				<p style="text-align: left">&nbsp;</p>
+				
+			
+                      
+                      
+                      </div>
+			<div id="body-right" style="width: 318; height: 252">
+				<h2>&nbsp;</h2>
+				<h2><font size="4" color="#A2B849">Punti di interesse</font></h2>
+				<p>&nbsp;</p>
+				<p>
+                &nbsp;</p>
+			<ul>
+                    <li>
+					<p style="text-align: left"><font size="2">Informazioni sul traffico </font> <A HREF="http://www.guidareinformati.it/tr/"><B>click here</B></A></p>
+                	</li>
+                    <li>
+					<p style="text-align: left">&nbsp;</p>
+                	</li>
+                	<li>
+					<p style="text-align: left"><font size="2">Leggi normative italiane sul trasporto </font> <A HREF="http://www.infrastrutturetrasporti.it/"><B>click here</B></A></p>
+                	</li>
+                	<li>
+					<p style="text-align: left">&nbsp;</p>
+                	</li>
+                	<li>
+					<p style="text-align: left"><font size="2">Incoterms </font> <A HREF="http://www.abc-transportsweb.net/incoterms/glossary.htm"><B>click here</B></A></p>
+                	</li>
+                	<li>
+					<p style="text-align: left">&nbsp;</p>
+                	</li>
+                	<li>
+					<p style="text-align: left"><font size="2">Mappe, percorsi ed itinerari </font> <A HREF="http://www.maporama.com"><B>click here</B></A></p>
+                	</li>
+                	<li>
+					<p style="text-align: left">&nbsp;</p>
+                	</li>
+                	<li>
+					<p style="text-align: left"><font size="2">Divieti di circolazione </font> <A HREF="http://www.teletransport.it/newweb/ita/pagine/divieti.asp"><B>click here</B></A></p>
+                	</li>
+                	<li>
+					<p style="text-align: left">&nbsp;</p>
+                	</li>
+                	<li>
+					<p style="text-align: left"><font size="2">Convertitore valuta </font> <A HREF="http://it.finance.yahoo.com"><B>click here</B></A></p>
+                	</li>
+                	<li>
+					<p style="text-align: left">&nbsp;</p>
+                	</li>
+                	<li>
+					<p style="text-align: left"><font size="2">Numeri di telefono di tutto il mondo </font> <A HREF="http://www.infobel.com"><B>click here</B></A></p>
+                	</li>                	                	                	
+                	<li>
+					<p style="text-align: left">&nbsp;</p>
+                	</li>                	                	                	
+				<li>
+					<p style="text-align: left"><font size="2">Misure </font> <A HREF="http://www.themeter.net"><B>click here</B></A></p>
+                	</li>  
+                </ul>
+                
+				<p>&nbsp;</p>
+				<p><font size="2">&nbsp;</font></p>
+				<p class="more">&nbsp;</p>
+				<div id="gallery">
+					&nbsp;</div>
+			</div>
+			<div class="clear"></div>
+		</div>
+	</div>
+	<div id="footer">
+		<p>&copy;Corebit 2007 all rights reserved.</p>
+	</div>
+</body>
+</html>
